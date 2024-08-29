5449564ab11c869b3274a8a01a7f51671c02ba20
ichbinderbar <idrovel@gmail.com>
2024-08-29 14:53:46 +0100

    add theme handler onClick to playerArea

--- src/components/PlayerArea/PlayerArea.jsx
+++ src/components/PlayerArea/PlayerArea.jsx
@@ -2,6 +2,7 @@ import CardsCollectedCounter from "../CardsCollected/CardsCollectedCounter";
 import CoinsEarnedCounter from "../CoinsEarned/CoinsEarnedCounter";
 import Hand from "../Hand/Hand";
 import "./PlayerArea.scss";
+import { useState, useEffect } from "react";
 
 export default function PlayerArea({
   player,
@@ -9,8 +10,29 @@ export default function PlayerArea({
   fishedCards,
   coins,
 }) {
+  const [theme, setTheme] = useState("light");
+
+  const handleClick = () => {
+    if (theme === "dark") {
+      setTheme("light");
+      localStorage.setItem("theme", "light");
+      return;
+    }
+    setTheme("dark");
+    localStorage.setItem("theme", "dark");
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("theme") === "ligh") {
+      setTheme("light");
+    }
+    if (localStorage.getItem("theme") === "dark") {
+      setTheme("dark");
+    }
+  }, []);
+
   return (
-    <div className="player-area">
+    <div onClick={handleClick} className={`player-area player-area--${theme}`}>
       <Hand player={player} handleHandCardSelection={handleHandCardSelection} />
       <div className="player-area__stat-container">
         <CardsCollectedCounter
