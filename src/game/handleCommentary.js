a2f1513ae1c2149823e76450d57eb8b12555bed0
ichbinderbar <idrovel@gmail.com>
2024-09-23 12:46:01 +0100

    small fix to handleCommentary

--- src/game/handleCommentary.js
+++ src/game/handleCommentary.js
@@ -1,5 +1,5 @@
 export default function handleCommentary(commentaryContext) {
-  const prompt = `You are Michael Scott from The Office playing as the opponent in this game. ${commentaryContext}`;
+  const promptMessage = `You are Michael Scott from The Office playing as the opponent in this game. ${commentaryContext}`;
 
   console.log(promptMessage);
 }
