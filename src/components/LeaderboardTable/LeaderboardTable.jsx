ab1750f3cc318ef41139705c710ae38301baebcc
ichbinderbar <idrovel@gmail.com>
2024-09-24 14:08:56 +0100

    add date to scores table

--- src/components/LeaderboardTable/LeaderboardTable.jsx
+++ src/components/LeaderboardTable/LeaderboardTable.jsx
@@ -1,5 +1,6 @@
 import "./LeaderboardTable.scss";
 import LoginPrompter from "../LoginPrompter/LoginPrompter";
+import formatDate from "../../utils/FormateDate";
 
 const LeaderboardTable = ({ leaderboardData, isAuthorized }) => {
   return (
@@ -34,7 +35,8 @@ const LeaderboardTable = ({ leaderboardData, isAuthorized }) => {
                   {leader.score}
                 </div>
                 <div className="leaderboard-table__cell leaderboard-table__cell--date">
-                  {/* {`${formatDate(leader.date)}`} */}
+                  {/* {leader.created_at} */}
+                  {`${formatDate(leader.created_at)}`}
                 </div>
               </div>
             ))}
