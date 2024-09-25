bc78fae801d7d81fb8ddcdf3be10f52581c7c7b5
ichbinderbar <idrovel@gmail.com>
2024-09-25 14:57:04 +0100

    format date

--- src/utils/formateDate.js
+++ src/utils/formateDate.js
@@ -1,16 +1,13 @@
 export default function formatDate(isoString) {
-  const date = new Date(isoString);
+  try {
+    const date = new Date(isoString);
 
-  if (isNaN(date.getTime())) {
+    if (isNaN(date.getTime())) {
+      return "Invalid date";
+    }
+
+    return date.toISOString().replace("T", " ").split(".")[0];
+  } catch (error) {
     return "Invalid date";
   }
-
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0"); // getMonth() is zero-based
-  const day = String(date.getDate()).padStart(2, "0");
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-
-  return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
 }
