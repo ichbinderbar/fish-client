6bf4a336e774e23ccc100f7c8434394e11afc4cb
ichbinderbar <idrovel@gmail.com>
2024-09-26 11:46:33 +0100

    add card and coin emoji

--- src/components/CardsCollected/CardsCollectedCounter.jsx
+++ src/components/CardsCollected/CardsCollectedCounter.jsx
@@ -5,6 +5,7 @@ export default function CardsCollectedCounter({ fishedCards }) {
   return (
     <div className="player-area__cards-collected-container">
       <img className="player-area__cards-img" src={deckImg} alt="" />
+      <div className="player-area__cards-emoji">🃏</div>
       <div className="player-area__cards-collected-counter">{fishedCards}</div>
     </div>
   );
--- src/components/CoinsEarned/CoinsEarnedCounter.jsx
+++ src/components/CoinsEarned/CoinsEarnedCounter.jsx
@@ -5,6 +5,7 @@ export default function CoinsEarnedCounter({ coins }) {
   return (
     <div className="player-area__coins-earned">
       <img className="player-area__coins-img" src={deckImg} alt="" />
+      <div className="player-area__cards-emoji">🪙</div>
       <div className="player-area__coins-earned-counter">{coins}</div>
     </div>
   );
