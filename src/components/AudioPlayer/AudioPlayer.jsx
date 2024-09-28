743c97b3a7ba9dcae0d49ac000d8502469ddd360
ichbinderbar <idrovel@gmail.com>
2024-09-29 00:21:37 +0100

    add songs

--- src/components/AudioPlayer/AudioPlayer.jsx
+++ src/components/AudioPlayer/AudioPlayer.jsx
@@ -4,6 +4,13 @@ import "./AudioPlayer.scss";
 export default function AudioPlayer() {
   const audioRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+
+  const songs = [
+    "/Cumbia Sobre el Mar.mp3",
+    "/Cumbia Del Olvido.mp3",
+    "/Manana Tepotzlan.mp3",
+  ];
 
   useEffect(() => {
     if (audioRef.current) {
@@ -11,22 +18,49 @@ export default function AudioPlayer() {
     }
   }, []);
 
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.addEventListener("ended", handleSongEnd);
+    }
+
+    return () => {
+      if (audioRef.current) {
+        audioRef.current.removeEventListener("ended", handleSongEnd);
+      }
+    };
+  }, [currentSongIndex]);
+
+  const handleSongEnd = () => {
+    setCurrentSongIndex((prevIndex) => (prevIndex + 1) % songs.length);
+  };
+
   const togglePlayPause = () => {
     if (audioRef.current) {
       if (isPlaying) {
         audioRef.current.pause();
       } else {
+        setCurrentSongIndex((prevIndex) => (prevIndex + 1) % songs.length);
+        audioRef.current.load();
         audioRef.current.play();
       }
       setIsPlaying(!isPlaying);
     }
   };
 
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.load();
+      if (isPlaying) {
+        audioRef.current.play();
+      }
+    }
+  }, [currentSongIndex, isPlaying]);
+
   return (
     <button className="audio-button" onClick={togglePlayPause}>
       {isPlaying ? "🔊" : "🔈"}
-      <audio ref={audioRef} loop>
-        <source src="/background-song.mp3" type="audio/mpeg" />
+      <audio ref={audioRef}>
+        <source src={songs[currentSongIndex]} type="audio/mpeg" />
         Your browser does not support the audio element.
       </audio>
     </button>
