fe2d752818d153d979409b40cc45b0f5f09aef9c
juanidrovo <jidrovo_intern@kriptos.io>
2025-06-25 00:56:33 -0500

    udpate instructions

--- src/components/InstructionsBoard/InstructionsBoard.jsx
+++ src/components/InstructionsBoard/InstructionsBoard.jsx
@@ -39,47 +39,53 @@ export default function InstructionsBoard({ theme }) {
               possible.
               <br />
               First select the cards you want to "fish" from the table, then
-              select the "hook"from your hand. The "hook" is the card you will
+              select the "hook" from your hand. The "hook" is the card you will
               use to capture the combination selected from the table.
               <br />
               The order in which you select the cards on the table does not
-              matter.
+              matter. Just make sure you do it before you choose the hook card.
             </p>
           </div>
           <div className="section">
             <h3>Goal:</h3>
-            <p>Earn 20 coins before your opponent to win the game.</p>
+            <p>Earn 40 coins before your opponent to win the game.</p>
             <br />
             <h3>Tip:</h3>
             <p>
-              There are 40 cards in total. The values of the cards go from 1 to
-              10. Therefore each number only repeats 4 times. Try to remember
-              the cards that have been played to better your odds of winning.
+              There are 40 cards in total. Therefore each card value only
+              repeats 4 times. Try to remember the cards that have been played
+              to better your odds of winning. For example, if you have seen the
+              Jack tree times already you can be sure it is safe to play it next
+              since it is imposible for your opponnent to fish it off the table
+              in the next move.
             </p>
           </div>
           <div className="section">
             <h3>Turns:</h3>
             <p>
-              On each turn, a player must attempt to capture as many cards from
-              the table as possible using a card from their hand.
+              On each turn, you must attempt to fish as many cards from the
+              table as possible using a card from your hand, the hook.
               <br />
-              There are two types of cards in the game: blacks (or high cards)
-              and colors (or low cards). If the played card is a black card, in
-              order to capture or "fish" a card from the table it must match its
-              value exactly.
+              There are two types of cards in the game: royals (or high cards:
+              J, Q, K) and colors (or low cards: A - 7). If the played card is a
+              royal card, in order to capture or "fish" a card from the table it
+              must match its value exactly.
               <br />
-              If the played card is a color card it can "fish" like black cards
-              or it can "fish" two cards by adding up their values (for example,
-              if the player has a card with a value of 3 and the table contains
-              the following cards: 1, 2, and 3, the player can either capture
-              the 3 alone with his 3 or the 1 and the 2 together).
+              If the played card is a low card (A - 7) it can "fish" like royals
+              or it can capture up to two cards at once by adding up their
+              values to match its own. For example, if you have a card with a
+              value of 3 and the table contains the following cards: 1, 2, and
+              3, you can either capture the 3 alone with your 3 or the 1 and the
+              2 together because 1 + 2 = 3.
               <br />
-              If you can "hook" a match as explained before all inmediatelly
-              consecutive cards after the "hook" can also be collected and
-              counted into the players cards pile (for example, if the player
-              has a card with a value of 3 and the table contains the following
-              cards: 1, 2, 3 and 4, the player is allowed to "fish" any of these
-              combinations off the table: [1, 2, 4], [3, 4], [1, 2] or [3]).
+              If you capture as explained before all inmediatelly consecutive
+              cards after the hook's card value can also be collected. For
+              example, if you have a 3 in your hand and the table contains the
+              following cards: 1, 2, 3 and 4, you could "fish" any of these
+              combinations off the table: [1, 2, 4] because 1 + 2 = 3 and 4
+              comes inmediatelly after 3, [3, 4] because 4 comes inmediatelly
+              after 3, [1, 2] because 1 + 2 = 3, or you could simply pick up the
+              [3] by itself.
               <br />
               If a player cannot "fish" any cards, they must discard a card onto
               the table. The turn then passes to the next player.
@@ -88,21 +94,21 @@ export default function InstructionsBoard({ theme }) {
           <div className="section">
             <h3>Points:</h3>
             <p>
-              1 coin can be earned for capturing the card discarted by the
+              2 coins will be awarded for capturing the card discarted by the
               previous player with a card of the same value inmediatelly after
-              it is placed on the table.
-              <br />1 coin is awarded if the player leaves the table empty after
-              capturing.
+              it is placed on the table. This is called a "caida".
+              <br />2 additional coins will be awarded if the player leaves the
+              table empty after capturing.
               <br />
-              This two scenarios can happen on the same turn on which case 2
-              points are awarded.
+              This two scenarios can happen on the same turn on which case a
+              total of 4 points will be awarded.
               <br />
               At the end of each round the cards collected during that round are
               counted and 6 coins are awarded for every 20 captured cards. One
-              additional coin is awarded for every 2 additional captured cards
-              (for example, a player whom at the end of the round has collected
-              23 cards will receive 6 coins for the first 20 and 1 coin for the
-              additional 3 cards, for a grand total of 7 coins).
+              additional coin is awarded for every additional captured card (for
+              example, a player whom at the end of the round has collected 23
+              cards will receive 6 coins for the first 20 and 3 coins for the
+              additional 3 cards, for a grand total of 9 coins).
             </p>
           </div>
           <div className="section">
@@ -121,8 +127,8 @@ export default function InstructionsBoard({ theme }) {
             <h3>Game Over:</h3>
             <p>
               The game can end at the end of a round when the cards are counted
-              and exchanged for coins when one of the players reaches 20 coins,
-              or it can end by a player reaching 20 coins at any point during
+              and exchanged for coins once one of the players reaches 40 coins,
+              or it can end by a player reaching 40 coins at any point during
               the game.
             </p>
           </div>
