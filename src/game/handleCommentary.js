7d1d681a7106539f54399672dd19de371fe99f5f
juanidrovo <jidrovo_intern@kriptos.io>
2025-06-25 02:15:10 -0500

    modify ai prompt

--- src/game/handleCommentary.js
+++ src/game/handleCommentary.js
@@ -1,32 +1,30 @@
 import axios from "axios";
 import { apiUrlOpenAi as apiUrl } from "../assets/data/Api";
 
-export default async function handleCommentary(
-  commentaryContext,
-  player,
-  opponent
-) {
+export default async function handleCommentary(commentaryContext, opponent) {
   const promptMessage = `
   ${new Date().toLocaleString()}
-  You are impersonating ${opponent.name} in this game.
-  Copy their talking style. Say things that person would say
-  like famous quotes or words they often use.
-  I am ${player.name}. We are not frienly to eachother.
-  Do not refer to me by name.
-  The game is a cards game where you collect cards from the table.
-  The goal of the game is to earn 20 coins.
+  You are ${
+    opponent.name
+  } in this game of cards. We are playing the ecuadorian game "Cuarenta".
+  Talk exactly like ${
+    opponent.name
+  } would while playing cards. Say things that ${
+    opponent.name
+  } would say. Use famous quotes or idioms used by ${opponent.name}.
+  The goal of the game is to earn 40 coins.
   You can gain coins during each round
   and also when your cards are exchanged for coins at the end of each round.
   You need at least 20 cards to earn some coins at the end of the round.
   Otherwise you don't get any additional coins.
-  You are already impersonating the character. Don't break character ever.
+  You are already impersonating ${opponent.name}. Don't break character ever.
   Including when responding to this first prompt.
-  Respond with two phrases max.
+  Respond with one phrase always.
   Pay attention to the timestamps to give yourself context of the evolution of the game.
   Pay attention to what you have said before so you don't repeat yourself.
   ${commentaryContext}`;
 
-  console.log(promptMessage);
+  // console.log(promptMessage);
 
   try {
     const result = await axios.post(`${apiUrl}/api/get-response`, {
